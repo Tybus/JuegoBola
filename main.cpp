985f106b534805d8fdc742720fe9fbc5d58b50bf
GabrielBarrantes <gabrielenrique.barrantes@gmail.com>
2018-07-05 21:33:47 -0600

    Update and rename main.c to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#define __FORE_GROUND_COLOR 0x00FFFF00
+#define __BALL_COLOR        0x000000FF
+
+extern "C"
+{
 #include <ti/devices/msp432p4xx/inc/msp.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include <ti/grlib/grlib.h>
@@ -5,26 +10,31 @@
 #include "HAL_OPT3001.h"
 #include "LcdDriver/Crystalfontz128x128_ST7735.h"
 #include "LcdDriver/HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h"
+}
+
 #include <stdio.h>
 #include <math.h>
+
+#include <Ball.hpp>
+#include <Laberynth.h>
 
 /* Graphic library context */
 Graphics_Context g_sContext;
 Graphics_Rectangle g_sRectangle;
-//uint16_t ax; //= 0U;
-//uint16_t ay; //= 0U;
-//uint16_t az; //= 0U;
 
-int ax=0;
-int ay=0;
-int az=0;
+int32_t g_iAccelerationX=0;
+int32_t g_iAccelerationY=0;
+int32_t g_iAccelerationZ=0;
 
-double vx=0;
-double vy=0;
-double vz=0;
+double g_dSpeedX=0;
+double g_dSpeedY=0;
+double g_dSpeedZ=0;
 
-double x=0;
-double y=0;
+#ifndef XY
+#define XY
+double x;
+double y;
+#endif
 double z=0;
 
 double xold=0;
@@ -55,7 +65,7 @@ void Setup(void)
     // - Start the timer in UP mode.
     // - Re-enable interrupts
     //TIMER32_1->LOAD = 0x002DC6C0;//TIMER32_COUNT; //~1ms ---> a 48Mhz
-    TIMER32_1->LOAD = 0x00000BB8;//TIMER32_COUNT;
+    TIMER32_1->LOAD = 0x0000BB80;//TIMER32_COUNT;
     TIMER32_1->CONTROL = TIMER32_CONTROL_SIZE | TIMER32_CONTROL_PRESCALE_0 | TIMER32_CONTROL_MODE | TIMER32_CONTROL_IE | TIMER32_CONTROL_ENABLE;
     NVIC_SetPriority(T32_INT1_IRQn,1);
     NVIC_EnableIRQ(T32_INT1_IRQn);
@@ -64,7 +74,6 @@ void Setup(void)
     // ****************************
     //       SCREEN CONFIG
     // ****************************
-    /////////////////////////////////////////////////////////////////////
     /* Initializes display */
     Crystalfontz128x128_Init();
 
@@ -75,7 +84,13 @@ void Setup(void)
     Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128,
                          &g_sCrystalfontz128x128_funcs);
 
-    Graphics_clearDisplay(&g_sContext);
+    Graphics_setForegroundColor(&g_sContext, __FORE_GROUND_COLOR);
+    //Graphics_drawRectangle(&g_sContext,&g_sRectangle);
+    g_sRectangle.xMax = 0; g_sRectangle.xMin = 127;
+    g_sRectangle.yMax = 0; g_sRectangle.yMin = 127;
+    Graphics_fillRectangle(&g_sContext,&g_sRectangle);
+
+    //Graphics_clearDisplay(&g_sContext);
 /*
     Graphics_setForegroundColor(&g_sContext, 0x000000FF);
     //Graphics_drawRectangle(&g_sContext,&g_sRectangle);
@@ -86,29 +101,18 @@ void Setup(void)
     Graphics_fillRectangle(&g_sContext,&g_sRectangle);
     //Graphics_drawLineH(&g_sContext, 0, 127, 63);
 
-    Graphics_setForegroundColor(&g_sContext, 0x00DAA520);
+    Graphics_setForegroundColor(&g_sContext, __FORE_GROUND_COLOR);
     //Graphics_drawRectangle(&g_sContext,&g_sRectangle);
-    g_sRectangle.xMax = 0;
-    g_sRectangle.xMin = 127;//127;
-    g_sRectangle.yMax = 64;
-    g_sRectangle.yMin = 127;//127;
+    g_sRectangle.xMax = 0; g_sRectangle.xMin = 127;
+    g_sRectangle.yMax = 0; g_sRectangle.yMin = 127;
     Graphics_fillRectangle(&g_sContext,&g_sRectangle);
+
+    //Graphics_setForegroundColor(&g_sContext, 0x00FF0000);
+    //Graphics_fillCircle(&g_sContext, 64, 64, 3);
 */
-    Graphics_setForegroundColor(&g_sContext, 0x00FF0000);
-    Graphics_fillCircle(&g_sContext, 64, 64, 3);
-
-
-
-
-
-    return;
-}
-
-
-
-
-void AccelerometerSetup()
-{
+    // ****************************
+    //     ACCELEROMETER CONFIG
+    // ****************************
     // Set P6.1 for Analog input (X-Axis).
     P6->SEL0 |= BIT1;
     P6->SEL1 |= BIT1;
@@ -140,8 +144,57 @@ void AccelerometerSetup()
     NVIC_SetPriority(ADC14_IRQn,1);
     NVIC_EnableIRQ(ADC14_IRQn);
 
+    return;
+}
+/////////////////////////
+///////////////////////
+/////////////////////////
+////////////////////////
+////////////////////////
+/*
+int32_t isOnCircle(int32_t xtest, int32_t ytest, int32_t xNew, int32_t yNew )
+{
+    if( (xNew-xtest<3 && xtest-xNew<3)&& (yNew-ytest<3 && ytest-yNew<3)){return 1;}
+    if( ((xtest==xNew-3 || xtest==xNew+3 ) && (yNew==ytest||yNew+1==ytest||yNew-1==ytest)) ){return 1;}
+    if( ((ytest==yNew-3 || ytest==yNew+3 ) && (xNew==xtest||xNew+1==xtest||xNew-1==xtest)) ){return 1;}
+    return 0;
 }
 
+
+void Graphics_deleteCircle(const Graphics_Context *context,int32_t currentx,int32_t currenty,
+                                                           int32_t newX,int32_t newY,
+                                                           int32_t lRadius)
+{
+    //Graphics_setForegroundColor(&g_sContext, 0x00000000);
+    Graphics_setForegroundColor(&g_sContext, __FORE_GROUND_COLOR);
+    //if (! isOnCircle(xTest, yTest,  x,  y )){Graphics_drawPixel(&g_sContext,xTest,yTest);}
+
+
+    int i;
+    int j;
+
+    for(i=-2;i<3;i+=1)
+    {
+        for(j=-2 ; j<3; j+=1)
+        {
+            if (isOnCircle(currentx+i, currenty+j,  newX,  newY )==0){Graphics_drawPixel(&g_sContext,currentx+i, currenty+j);}
+        }
+    }
+
+    for(i=-1;i<2;i+=1)
+    {
+        if (isOnCircle(currentx+i, currenty+3,  newX,  newY )==0){Graphics_drawPixel(&g_sContext,currentx+i, currenty+3);}
+        if (isOnCircle(currentx+i, currenty-3,  newX,  newY )==0){Graphics_drawPixel(&g_sContext,currentx+i, currenty-3);}
+        if (isOnCircle(currentx+3, currenty+i,  newX,  newY )==0){Graphics_drawPixel(&g_sContext,currentx+3, currenty+i);}
+        if (isOnCircle(currentx-3, currenty+i,  newX,  newY )==0){Graphics_drawPixel(&g_sContext,currentx-3, currenty+i);}
+    }
+}*/
+
+/////////////////////////
+///////////////////////
+/////////////////////////
+////////////////////////
+////////////////////////
 
 /*
  * Main function
@@ -149,9 +202,10 @@ void AccelerometerSetup()
 int main(void)
 {
     Setup();
-    AccelerometerSetup();
+    x=0;
+    y=0;
 
-
+    Ball ball(__BALL_COLOR ,&g_sContext);
 
     while(1)
     {
@@ -159,35 +213,39 @@ int main(void)
         // Trigger the conversion in the ADC,
         // the ADC14_IRQHandler( ) procedure run at the end of conversion
         //ADC14->CTL0 = ADC14->CTL0 | ADC14_CTL0_SC;
-
+/*
         if((int32_t) round(x) > 54 || (int32_t) round(x) < -54){vx=0; }
         if((int32_t) round(y) > 54 || (int32_t) round(y) < -54){vy=0; }
 
-        Graphics_setForegroundColor(&g_sContext, 0x00000000);
-        Graphics_fillCircle(&g_sContext, ((int32_t) round(xold))+64, ((int32_t) round(yold))+64, 4);
-
-
-
+        //Graphics_setForegroundColor(&g_sContext, 0x00000000);
+        //Graphics_fillCircle(&g_sContext, ((int32_t) round(xold))+64, ((int32_t) round(yold))+64, 3);
 
         __disable_irq();
-        Graphics_setForegroundColor(&g_sContext, 0x00FF0000);
-        Graphics_fillCircle(&g_sContext, ((int32_t) round(x))+64, ((int32_t) round(y))+64, 4);
+        Graphics_deleteCircle(&g_sContext,((int32_t) round(xold))+64,((int32_t) round(yold))+64,
+                              ((int32_t) round(x))+64, ((int32_t) round(y))+64,3);
+
+
+        Graphics_setForegroundColor(&g_sContext, __BALL_COLOR);
+        Graphics_fillCircle(&g_sContext, ((int32_t) round(x))+64, ((int32_t) round(y))+64, 3);
         xold=x;
         yold=y;
         __enable_irq();
+*/
 
+        ball.RefreshOnScreen();
     }
 }
 
-
+extern "C"
+{
 
 void ADC14_IRQHandler(void)
 {
     __disable_irq();
 
-    ax=ADC14->MEM[0]-8192;
-    ay=ADC14->MEM[1]-8192;
-    az=ADC14->MEM[2]-8192;
+    g_iAccelerationX=ADC14->MEM[0]-8192;
+    g_iAccelerationY=ADC14->MEM[1]-8192;
+    g_iAccelerationZ=ADC14->MEM[2]-8192;
 
     ADC14->CLRIFGR0 = ADC14_CLRIFGR0_CLRIFG0;
     __enable_irq();
@@ -200,13 +258,12 @@ void T32_INT1_IRQHandler(void)
     //P1->OUT ^= BIT0; // - Toggle the heart beat indicator (1ms)
 
     ADC14->CTL0 = ADC14->CTL0 | ADC14_CTL0_SC;
-    vx+=ax*0.001*0.1;//0.0010986;
-    vy+=-ay*0.001*0.1;//0.0010986;
+    g_dSpeedX+= g_iAccelerationX*0.001*0.1;//0.0010986;
+    g_dSpeedY+=-g_iAccelerationY*0.001*0.1;//0.0010986;
 
-    x+=vx*0.001;
-    y+=vy*0.001;
-
+    x+=g_dSpeedX*0.001;
+    y+=g_dSpeedY*0.001;
     //Graphics_fillCircle(&g_sContext, ((int32_t) (x))+64, ((int32_t) (y))+64, 4);
-
     return;
+}
 }
